aa3e35f02b9ad32dfc35a19824872a0226f654d1
aldoBonilla <aldo.bonilla@definityfirst.com>
2015-05-19 10:59:32 -0500

    Initial commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Aldo R Bonilla Guerrero on 19/05/15.
+//  Copyright (c) 2015 Aldo R Bonilla Guerrero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
